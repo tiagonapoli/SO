9314ce2f901750616a0de976a97be14091cd05d8
ricardokojo <ricardokojo7@gmail.com>
2018-10-21 03:13:32 -0300

    Add EP2 - Parte 1 changes
    Update proto.h
        _PROTOTYPE of privileges_dmp
    Update dmp.c
        Link privileges_dmp to F4 key
    Update dmp_kernel.c
        Add function privileges_dmp (sorting still missing)
    
    Signed-off-by: ricardokojo <ricardokojo7@gmail.com>

--- EP2/usr/src/servers/is/dmp.c
+++ EP2/usr/src/servers/is/dmp.c
@@ -14,56 +14,58 @@
  */
 #define NHOOKS 18
 
-struct hook_entry {
+struct hook_entry
+{
 	int key;
 	void (*function)(void);
 	char *name;
 } hooks[NHOOKS] = {
-	{ F1, 	proctab_dmp, "Kernel process table" },
-	{ F2,   memmap_dmp, "Process memory maps" },
-	{ F3,	image_dmp, "System image" },
-	{ F4,	privileges_dmp, "Process privileges" },
-	{ F5,	monparams_dmp, "Boot monitor parameters" },
-	{ F6,	irqtab_dmp, "IRQ hooks and policies" },
-	{ F7,	kmessages_dmp, "Kernel messages" },
-	{ F9,	sched_dmp, "Scheduling queues" },
-	{ F10,	kenv_dmp, "Kernel parameters" },
-	{ F11,	timing_dmp, "Timing details (if enabled)" },
-	{ SF1,	mproc_dmp, "Process manager process table" },
-	{ SF2,	sigaction_dmp, "Signals" },
-	{ SF3,	fproc_dmp, "Filesystem process table" },
-	{ SF4,	dtab_dmp, "Device/Driver mapping" },
-	{ SF5,	mapping_dmp, "Print key mappings" },
-	{ SF6,	rproc_dmp, "Reincarnation server process table" },
-	{ SF7,  holes_dmp, "Memory free list" },
-	{ SF8,  data_store_dmp, "Data store contents" },
+	{F1, proctab_dmp, "Kernel process table"},
+	{F2, memmap_dmp, "Process memory maps"},
+	{F3, image_dmp, "System image"},
+	/* ########################################################### */
+	{F4, priority_dmp, "Process priorities - EP2"},
+	/* ########################################################### */
+	{F5, monparams_dmp, "Boot monitor parameters"},
+	{F6, irqtab_dmp, "IRQ hooks and policies"},
+	{F7, kmessages_dmp, "Kernel messages"},
+	{F9, sched_dmp, "Scheduling queues"},
+	{F10, kenv_dmp, "Kernel parameters"},
+	{F11, timing_dmp, "Timing details (if enabled)"},
+	{SF1, mproc_dmp, "Process manager process table"},
+	{SF2, sigaction_dmp, "Signals"},
+	{SF3, fproc_dmp, "Filesystem process table"},
+	{SF4, dtab_dmp, "Device/Driver mapping"},
+	{SF5, mapping_dmp, "Print key mappings"},
+	{SF6, rproc_dmp, "Reincarnation server process table"},
+	{SF7, holes_dmp, "Memory free list"},
+	{SF8, data_store_dmp, "Data store contents"},
 };
 
 /*===========================================================================*
  *				handle_fkey				     *
  *===========================================================================*/
-#define pressed(k) ((F1<=(k)&&(k)<=F12 && bit_isset(m->FKEY_FKEYS,((k)-F1+1)))\
-  	|| (SF1<=(k) && (k)<=SF12 && bit_isset(m->FKEY_SFKEYS, ((k)-SF1+1)))) 
+#define pressed(k) ((F1 <= (k) && (k) <= F12 && bit_isset(m->FKEY_FKEYS, ((k)-F1 + 1))) || (SF1 <= (k) && (k) <= SF12 && bit_isset(m->FKEY_SFKEYS, ((k)-SF1 + 1))))
 PUBLIC int do_fkey_pressed(m)
-message *m;					/* notification message */
+	message *m; /* notification message */
 {
-  int s, h;
+	int s, h;
 
-  /* The notification message does not convey any information, other
+	/* The notification message does not convey any information, other
    * than that some function keys have been pressed. Ask TTY for details.
    */
-  m->m_type = FKEY_CONTROL;
-  m->FKEY_REQUEST = FKEY_EVENTS;
-  if (OK != (s=sendrec(TTY_PROC_NR, m)))
-      report("IS", "warning, sendrec to TTY failed", s);
+	m->m_type = FKEY_CONTROL;
+	m->FKEY_REQUEST = FKEY_EVENTS;
+	if (OK != (s = sendrec(TTY_PROC_NR, m)))
+		report("IS", "warning, sendrec to TTY failed", s);
 
-  /* Now check which keys were pressed: F1-F12, SF1-SF12. */
-  for(h=0; h < NHOOKS; h++)
-      if(pressed(hooks[h].key))
-          hooks[h].function();
+	/* Now check which keys were pressed: F1-F12, SF1-SF12. */
+	for (h = 0; h < NHOOKS; h++)
+		if (pressed(hooks[h].key))
+			hooks[h].function();
 
-  /* Don't send a reply message. */
-  return(EDONTREPLY);
+	/* Don't send a reply message. */
+	return (EDONTREPLY);
 }
 
 /*===========================================================================*
@@ -73,30 +75,28 @@ PRIVATE char *key_name(int key)
 {
 	static char name[15];
 
-	if(key >= F1 && key <= F12)
+	if (key >= F1 && key <= F12)
 		sprintf(name, " F%d", key - F1 + 1);
-	else if(key >= SF1 && key <= SF12)
+	else if (key >= SF1 && key <= SF12)
 		sprintf(name, "Shift+F%d", key - SF1 + 1);
 	else
 		sprintf(name, "?");
 	return name;
 }
 
-
 /*===========================================================================*
  *				mapping_dmp				     *
  *===========================================================================*/
 PUBLIC void mapping_dmp(void)
 {
-  int h;
+	int h;
 
-  printf("Function key mappings for debug dumps in IS server.\n");
-  printf("        Key   Description\n");
-  printf("-------------------------------------");
-  printf("------------------------------------\n");
+	printf("Function key mappings for debug dumps in IS server.\n");
+	printf("        Key   Description\n");
+	printf("-------------------------------------");
+	printf("------------------------------------\n");
 
-  for(h=0; h < NHOOKS; h++)
-      printf(" %10s.  %s\n", key_name(hooks[h].key), hooks[h].name);
-  printf("\n");
+	for (h = 0; h < NHOOKS; h++)
+		printf(" %10s.  %s\n", key_name(hooks[h].key), hooks[h].name);
+	printf("\n");
 }
-
